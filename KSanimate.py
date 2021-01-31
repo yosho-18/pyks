2604f66251a505bb51d215544537634e3de883de
yosho <44283410+yosho@users.noreply.github.com>
2021-02-01 03:23:13 +0900

    update

--- KSanimate.py
+++ KSanimate.py
@@ -18,7 +18,7 @@ u = u - u.mean()
 ks.xspec[0] = np.fft.rfft(u)
 
 # time stepping loop.
-nmin = 1000; nmax = 5000
+nmin = 1000; nmax = 20000#10001
 uu = []; tt = []
 vspec = np.zeros(ks.xspec.shape[1], np.float)
 x = np.arange(N)
@@ -61,12 +61,84 @@ plt.contourf(x,tt[:nplt],uu[:nplt],31,extend='both')
 plt.xlabel('x')
 plt.ylabel('t')
 plt.colorbar()
-plt.title('chaotic solution of the K-S equation')
+#plt.title('chaotic solution of the K-S equation')
+plt.savefig("K-S_exact.png")
+plt.savefig("K-S_exact.eps")
 
-plt.figure()
+"""plt.figure()
 plt.loglog(ks.wavenums,vspec)
 plt.title('variance spectrum')
 plt.ylim(0.001,10000)
 plt.xlim(0,100)
-
+"""
 plt.show()
+
+
+import warnings
+
+warnings.filterwarnings('ignore')
+
+import csv
+import numpy as np
+
+numICs = 1#0000
+
+x1range = [-2, 2]
+x2range = [-2, 2]
+x3range = [-2, 2]
+#tSpan = np.arange(0, 2.5 + 0.1, 0.25)# np.arange(0, 125, 0.25)  # 0:0.02:1
+tSpan = np.arange(0, 160 + 0.0001, 0.001)
+
+
+
+def make_csv(filename, X):
+    with open(filename, 'w') as csv_file:
+        fieldnames = []
+        for i in range(128):
+            fieldnames.append('prediction_x' + "{stp:02}".format(stp=i))
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        dic = {}
+        for i in range(len(X)):
+            for j in range(128):
+                dic[fieldnames[j]] = X[i][j]
+            writer.writerow(dic)
+
+
+
+
+z = uu#[:nplt]
+z = np.array(z)
+
+X_train = z
+###############Discrete_Linear#############################
+filenamePrefix = 'K_S_FFT'
+"""
+#tSpan = np.arange(0, 12.5, 0.25)  # 0, 12.5, 0.25
+tSpan = np.arange(0, 2100 - 0.0001, 0.001)  # 160
+seed = 10
+#X_train = Kuramoto_Sivashinsky_ODE(x1range, x2range, x3range, numICs, tSpan, seed, "")
+#tSpan = np.arange(0, 2.5 - 0.1, 0.25)
+X_train = z[:-1]
+filename_train = filenamePrefix + '_train_x.csv'
+make_csv(filename_train, X_train)
+
+#seed = 10
+tSpan = np.arange(0, 2100 + 0.0001, 0.001)
+#tSpan = np.arange(0, 2.5 + 0.1, 0.25) # 0, 2.5 + 0.1, 0.25
+filename_train = filenamePrefix + '_train_y.csv'
+X_train = z[1:]
+make_csv(filename_train, X_train)
+
+#seed = 1#1
+#tSpan = np.arange(0, 12.5, 0.25)
+tSpan = np.arange(0, 2100 + 0.0001, 0.001)
+filename_train = filenamePrefix + "_E_recon_50" + '.csv'
+X_train = z[:-1]
+make_csv(filename_train, X_train)
+"""
+#seed = 3
+#tSpan = np.arange(0, 0.25 + 0.1, 0.25)  # 0, 12.5, 0.25
+tSpan = np.arange(0, 100 + 0.001, 0.001)
+filename_train = filenamePrefix + "_E_eigfunc" + '.csv'
+X_train = z#[:-1]
+make_csv(filename_train, X_train)
